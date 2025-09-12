af18cd385b175639cc21aea863d890c0245a8937
sigmotoa <si.galvismotoa@gmail.com>
2025-09-12 13:14:33 -0500

    Pedir año de nacimiento

--- Edad.java
+++ Edad.java
@@ -6,6 +6,7 @@ public class Edad{
 
     System.out.println("Ingrese su nombre");
     String name = leer.nextLine();
-
+	System.out.println(name+" ingrese su nacimiento");
+	int year=leer.nextInt();
   }
 }
