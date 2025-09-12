bd6495cdd8e4c04989d98dda5291c2d16605b190
sigmotoa <si.galvismotoa@gmail.com>
2025-09-12 12:54:42 -0500

    Primer commit

---
+++ Edad.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Edad{
+  public static void main(String args[]){
+
+    Scanner leer = new Scanner(System.in);
+
+    System.out.println("Ingrese su nombre");
+    String name = leer.nextLine();
+
+  }
+}
