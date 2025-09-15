b27b17a69ffc46bd28d618494095b3e0149b4f08
Sergio Iván Galvis Motoa <github@sigmotoa.com>
2025-09-15 17:08:22 -0500

    Update Edad.java
    Calcular menor edad

--- Edad.java
+++ Edad.java
@@ -4,9 +4,18 @@ public class Edad{
 
     Scanner leer = new Scanner(System.in);
 
+	byte current_year = 2025;  
     System.out.println("Ingrese su nombre");
     String name = leer.nextLine();
 	System.out.println(name+" ingrese su nacimiento");
 	int year=leer.nextInt();
+	byte age = current_year-year;
+	if (age<18){
+		System.out.println("Bye");
+	}
+	  else{
+	  System.out.println("Welcome");
+	  }
+	  
   }
 }
